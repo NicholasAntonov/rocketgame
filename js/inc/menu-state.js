d9db671c9a31e2a8f7e25e6480e11566a4db4e27
Pat Grasso <pgrasso@stevens.edu>
2015-02-16 01:02:30 -0500

    Add flare to the back of the rocket (when thrusting). Also linted most modules

--- js/inc/menu-state.js
+++ js/inc/menu-state.js
@@ -1,32 +1,33 @@
+/*global define, app*/
+
 define(['Phaser'], function (Phaser) {
+    'use strict';
+    var menuState = function (game) {};
 
-	var menuState = function(game){};
+    menuState.prototype = {
+        create : function () {
+            var game = app.game;
 
-	menuState.prototype = {
-		create : function () {
-			var game = app.game;
+            game.stage.backgroundColor = '#5E3F6B';
 
-			game.stage.backgroundColor = '#5E3F6B';
+            this.icon = game.add.sprite(game.world.centerX, game.world.centerY - 100, 'icon');
+            this.icon.anchor.setTo(0.5, 0.5);
 
-			this.icon = game.add.sprite(game.world.centerX, game.world.centerY - 100, 'icon');
-		    this.icon.anchor.setTo(0.5,0.5);
+            this.button = game.add.button(game.world.centerX, game.world.centerY + 100, 'startButton', function () { game.state.start('play'); }, this);
+            this.button.anchor.setTo(0.5, 0.5);
 
-		    this.button = game.add.button(game.world.centerX, game.world.centerY + 100, 'startButton', function () { game.state.start('play'); }, this);
-		    this.button.anchor.setTo(0.5,0.5);
+            this.buttontext = game.add.text(0, 0, "Start", { font: "32px Arial", fill: "#FFFFFF", align: "center" });
+            this.buttontext.anchor.setTo(0.5, 0.5);
 
-		    this.buttontext = game.add.text(0, 0, "Start", { font: "32px Arial", fill: "#FFFFFF", align: "center" });
-		    this.buttontext.anchor.setTo(0.5,0.5);
+            this.button.addChild(this.buttontext);
 
-		    this.button.addChild(this.buttontext);
+        },
 
-		},
+        update : function () {
+            var game = app.game;
+        }
 
-		update : function () {
-			var game = app.game;
+    }
 
-		}
-
-	}
-
-	return menuState;
+    return menuState;
 })
--- js/inc/preload.js
+++ js/inc/preload.js
@@ -1,16 +1,19 @@
+/*global define, app*/
+
 define(['Phaser'], function () {
+    'use strict';
 
+    function preload() {
+        var game = app.game;
 
-	function preload () {
-		var game = app.game;
+        game.load.image('icon', 'phaser.png');
+        game.load.image('rocket', '/images/sprites/playerShip1_blue.png');
+        game.load.image('rocket-flare', '/images/sprites/Effects/fire06.png');
+        game.load.image('small-star', '/images/sprites/Background/starSmall.png');
+        game.load.image('big-star', '/images/sprites/Background/starBig.png');
+        game.load.image('startButton', '/images/ui/blue_button00.png');
+        game.load.image('meteor', '/images/sprites/Meteors/meteorBrown_big4.png');
+    }
 
-		game.load.image('icon', 'phaser.png');
-		game.load.image('rocket', '/images/sprites/playerShip1_blue.png');
-		game.load.image('small-star', '/images/sprites/Background/starSmall.png');
-		game.load.image('big-star', '/images/sprites/Background/starBig.png');
-		game.load.image('startButton', '/images/ui/blue_button00.png');
-		game.load.image('meteor', '/images/sprites/Meteors/meteorBrown_big4.png');
-	}
-
-	return preload;
+    return preload;
 })
