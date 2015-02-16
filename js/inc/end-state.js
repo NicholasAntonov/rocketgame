6ed49e570b08bf41d36edda5657e253ff475b688
Nicholas Antonov <nicholasantonov@gmail.com>
2015-02-16 16:50:55 -0500

    text

--- js/inc/end-state.js
+++ js/inc/end-state.js
@@ -10,7 +10,13 @@ define(['Phaser'], function (Phaser) {
 
             game.stage.backgroundColor = '#5E3F6B';
 
+            this.buttontext = game.add.text(250, 100, 'You Win!', { font: "32px Arial", fill: "#FFFFFF", align: "center" });
+            this.buttontext.fixedToCamera = true;
+
             this.buttontext = game.add.text(200, 300, 'Your score was ' + app.score, { font: "32px Arial", fill: "#FFFFFF", align: "center" });
+            this.buttontext.fixedToCamera = true;
+
+            this.buttontext = game.add.text(220, 500, 'Press F5 to restart', { font: "32px Arial", fill: "#FFFFFF", align: "center" });
             this.buttontext.fixedToCamera = true;
 
         },
