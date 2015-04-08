016827c51f855f17f9e5bfb5d485fe240b5978be
patgrasso <pgrasso@stevens.edu>
2015-04-08 16:14:22 -0400

    Edit paths for github.io demo Change image paths from /images... to /rocketgame/images to satisfy github.io

--- js/inc/preload.js
+++ js/inc/preload.js
@@ -7,14 +7,14 @@ define(['Phaser'], function () {
         var game = app.game;
 
         game.load.image('icon', 'phaser.png');
-        game.load.image('rocket', '/images/sprites/playerShip1_blue.png');
-        game.load.image('rocket-flare', '/images/sprites/Effects/fire06.png');
-        game.load.image('small-star', '/images/sprites/Background/starSmall.png');
-        game.load.image('big-star', '/images/sprites/Background/starBig.png');
-        game.load.image('startButton', '/images/ui/blue_button00.png');
-        game.load.image('meteor', '/images/sprites/Meteors/meteorBrown_big4.png');
-        game.load.image('goldstar', '/images/platformerpack/Base pack/Items/star.png')
+        game.load.image('rocket', '/rocketgame/images/sprites/playerShip1_blue.png');
+        game.load.image('rocket-flare', '/rocketgame/images/sprites/Effects/fire06.png');
+        game.load.image('small-star', '/rocketgame/images/sprites/Background/starSmall.png');
+        game.load.image('big-star', '/rocketgame/images/sprites/Background/starBig.png');
+        game.load.image('startButton', '/rocketgame/images/ui/blue_button00.png');
+        game.load.image('meteor', '/rocketgame/images/sprites/Meteors/meteorBrown_big4.png');
+        game.load.image('goldstar', '/rocketgame/images/platformerpack/Base pack/Items/star.png');
     }
 
     return preload;
-})
+});
